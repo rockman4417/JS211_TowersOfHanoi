dab9ec9ce50463417ef23e429d6b8a0d5e8ede92
rockman4417 <67634648+rockman4417@users.noreply.github.com>
2020-11-07 13:11:37 -0600

    added JS logic and gui

--- main.js
+++ main.js
@@ -1,108 +1,128 @@
-'use strict';
-
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
-
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
 };
 
-// Start here. What is this function doing?
+let selectedIndex
+let selectedStack
+let targetStack
+
 const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-// Next, what do you think this function should do?
-const movePiece = () => {
+const movePiece = (startStack, endStack) => {
   // Your code here
+  //starStack pop last index
+  let movedPiece = startStack.pop(startStack.length - 1)
+  console.log("movedPiece" , movedPiece)
+  console.log("startStack", startStack)
+    endStack.push(movedPiece)
+    console.log("endStack" , endStack)
+  //endStack array push last index
 
 }
 
-// Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
+const isLegal = (startStack, endStack) => {
+    console.log("startStack" , startStack)
+    console.log("endStack", endStack)
   // Your code here
+  
+ if(startStack[startStack.length -1] != selectedIndex) {
+     console.log(startStack[startStack.length -1])
+     console.log(selectedIndex)
+    console.log("Not a Legal Move!  You selected the wrong index!" , stacks)
+    return false
+ }
+  
+  if(startStack[startStack.length - 1] > endStack[endStack.length - 1]) {
 
+    console.log("Not a Legal Move!  You tried to put a big piece on a small piece!" , stacks)
+    return false
+  }
+
+  if(endStack.value === undefined) {
+    return true
+}
+  
+else{
+    
+    return true
+    
+}
 }
 
-// What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
   // Your code here
-
-}
-
-// When is this function called? What should it do with its argument?
-const towersOfHanoi = (startStack, endStack) => {
-  // Your code here
-
-}
-
-const getPrompt = () => {
-  printStacks();
-  rl.question('start stack: ', (startStack) => {
-    rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
-      getPrompt();
-    });
-  });
-}
-
-// Tests
-
-if (typeof describe === 'function') {
-
-  describe('#towersOfHanoi()', () => {
-    it('should be able to move a block', () => {
-      towersOfHanoi('a', 'b');
-      assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
-    });
-  });
-
-  describe('#isLegal()', () => {
-    it('should not allow an illegal move', () => {
-      stacks = {
-        a: [4, 3, 2],
-        b: [1],
-        c: []
-      };
-      assert.equal(isLegal('a', 'b'), false);
-    });
-    it('should allow a legal move', () => {
+  //check the length of final array
+  if(stacks.c.length === 4) {
+      window.alert("You won!")
       stacks = {
         a: [4, 3, 2, 1],
         b: [],
         c: []
       };
-      assert.equal(isLegal('a', 'c'), true);
-    });
-  });
-  describe('#checkForWin()', () => {
-    it('should detect a win', () => {
-      stacks = { a: [], b: [4, 3, 2, 1], c: [] };
-      assert.equal(checkForWin(), true);
-      stacks = { a: [1], b: [4, 3, 2], c: [] };
-      assert.equal(checkForWin(), false);
-    });
-  });
+      console.log(stacks)
+    }
+}
 
-} else {
-
-  getPrompt();
+const towersOfHanoi = (startStack, endStack) => {
+  // Your code here
+  //check isLegal
+  
+    //movePiece
+    movePiece(startStack, endStack)
+    //checkForWin
+    checkForWin()
+    console.log("Piece Moved!" , stacks)
+  
+  
+  
 
 }
+
+
+
+//EVENT LISTENERS FOR THE DRAG AND DROP FUNCTIONALITY
+
+function onDragStart(event) {
+    selectedIndex = event.target.id
+    selectedStack = event.target.parentElement.id
+    event
+      .dataTransfer
+      .setData('text/plain', event.target.id);
+      console.log(selectedIndex)
+      console.log(selectedStack)
+
+      
+  }
+
+  function onDragOver(event) {
+    event.preventDefault();
+  }
+
+  function onDrop(event) {
+    
+    const id = event
+      .dataTransfer
+      .getData('text');
+
+      const draggableElement = document.getElementById(id);
+
+      const dropzone = event.target;
+      targetStack = dropzone.id
+      console.log(targetStack)
+      if(isLegal(stacks[selectedStack], stacks[targetStack])) {
+      towersOfHanoi(stacks[selectedStack], stacks[targetStack])
+
+      dropzone.appendChild(draggableElement);
+
+      
+      }
+      event
+    .dataTransfer
+    .clearData();
+  }
